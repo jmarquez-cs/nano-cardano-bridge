a0b00db9e63517dec4f015f19cdaaa9008dcb3f0
John <jmarquez.cs@gmail.com>
2021-08-02 02:53:11 -0400

    port back to 8080 (gcloud app deploy)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const helmet = require('helmet');
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 8080;
 const { getDb } = require('./firebase/firebase');
 const db = getDb();
 const { BridgePairs } = require('./firebase/collections');
